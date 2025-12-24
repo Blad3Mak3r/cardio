ff4f954ec099cbd7045f093e283a260f9b92f782
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-12-24 01:24:04 +0100

    Actualizar Extensions.kt

--- cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/Extensions.kt
+++ cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/Extensions.kt
@@ -3,7 +3,7 @@ package io.github.blad3mak3r.cardio.postgres
 import io.r2dbc.spi.Row
 
 inline fun <reified T> Row.getAsNullable(name: String): T? {
-    return this.get(name) as T?
+    return this.get(name, T::class.java)
 }
 
 inline fun <reified T> Row.getAs(name: String): T {
