3302367d460474d55bd8ede28967a98ead8b6de6
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-12-24 02:57:39 +0100

    Actualizar Extensions.kt

--- cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/Extensions.kt
+++ cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/Extensions.kt
@@ -1,6 +1,16 @@
 package io.github.blad3mak3r.cardio.postgres
 
+import io.r2dbc.spi.Connection
 import io.r2dbc.spi.Row
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+
+suspend inline fun <T> Connection.use(block: suspend (Connection) -> T): T {
+    return try {
+        block(this)
+    } finally {
+        close().awaitFirstOrNull()
+    }
+}
 
 inline fun <reified T> Row.getAsNullable(name: String): T? {
     return this.get(name, T::class.java)
