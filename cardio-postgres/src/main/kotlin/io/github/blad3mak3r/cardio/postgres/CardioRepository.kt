b790ca97e5a3e9178080da71aebc6d71282c858e
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-12-25 06:22:09 +0100

    Refactor CardioRepository methods from internal to protected visibility

--- cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/CardioRepository.kt
+++ cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/CardioRepository.kt
@@ -5,11 +5,11 @@ import io.r2dbc.spi.RowMetadata
 
 open class CardioRepository<C : Cardio>(val db: C) {
 
-    internal suspend fun <T> transaction(block: suspend CardioTransaction.() -> T): T {
+    protected suspend fun <T> transaction(block: suspend CardioTransaction.() -> T): T {
         return db.inTransaction(block)
     }
 
-    internal suspend fun <T> query(
+    protected suspend fun <T> query(
         stmt: String,
         args: List<Any?> = emptyList(),
         transform: (Row, RowMetadata) -> T
@@ -20,7 +20,7 @@ open class CardioRepository<C : Cardio>(val db: C) {
         }
     }
 
-    internal suspend fun execute(stmt: String, args: List<Any?> = emptyList()): Long {
+    protected suspend fun execute(stmt: String, args: List<Any?> = emptyList()): Long {
         return db.withConnection { connection ->
             val tx = CardioTransaction(connection)
             tx.execute(stmt, args)
