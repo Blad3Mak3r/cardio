0dd390d8110504eb3daa771d4084901e9248c4c9
Blad3Mak3r <juanluiscarobenito@gmail.com>
2026-02-26 14:37:29 +0100

    docs: Row#getAs Row#getAsNullable

--- cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/Extensions.kt
+++ cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/Extensions.kt
@@ -2,6 +2,7 @@ package io.github.blad3mak3r.cardio.postgres
 
 import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.sqlclient.SqlConnection
+import kotlinx.coroutines.Deferred
 
 typealias Row = io.vertx.sqlclient.Row
 
@@ -13,10 +14,20 @@ suspend fun <T> SqlConnection.use(block: suspend (SqlConnection) -> T): T {
     }
 }
 
+/**
+ * Extension function to get a column value as a nullable type.
+ * It uses the reified type parameter to determine the expected return type.
+ * If the column value is null, it will return null; otherwise, it will return the value cast to the expected type.
+ */
 inline fun <reified T> Row.getAsNullable(name: String): T? {
     return this.get(T::class.java, name)
 }
 
+/**
+ * Extension function to get a column value as a non-nullable type.
+ * It uses the reified type parameter to determine the expected return type.
+ * If the column value is null, it will throw an exception; otherwise, it will return the value cast to the expected type.
+ */
 inline fun <reified T> Row.getAs(name: String): T {
     return getAsNullable(name) ?: error("Column '$name' is null")
 }
