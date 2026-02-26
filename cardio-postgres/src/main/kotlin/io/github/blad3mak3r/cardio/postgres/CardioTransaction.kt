40d8ba6da5118b795b74fb948f72cdf3fcd0e988
Blad3Mak3r <juanluiscarobenito@gmail.com>
2026-02-26 13:14:55 +0100

    refactor: introduce extension function to convert List to Tuple for prepared queries

--- cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/CardioTransaction.kt
+++ cardio-postgres/src/main/kotlin/io/github/blad3mak3r/cardio/postgres/CardioTransaction.kt
@@ -1,11 +1,17 @@
 package io.github.blad3mak3r.cardio.postgres
 
+import io.vertx.core.buffer.Buffer
 import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.sqlclient.SqlConnection
 import io.vertx.sqlclient.Tuple
 import kotlin.coroutines.CoroutineContext
 
 class CardioTransaction(val c: SqlConnection) {
+
+    companion object {
+        private fun List<Any?>.toTuple(): Tuple =
+            Tuple.wrap(this.map { if (it is ByteArray) Buffer.buffer(it) else it })
+    }
 
     data class Context(val tx: CardioTransaction) : CoroutineContext.Element {
         companion object Key : CoroutineContext.Key<Context>
@@ -22,7 +28,7 @@ class CardioTransaction(val c: SqlConnection) {
         val result = if (args.isEmpty()) {
             c.query(stmt).execute().coAwait()
         } else {
-            c.preparedQuery(stmt).execute(Tuple.wrap(args)).coAwait()
+            c.preparedQuery(stmt).execute(args.toTuple()).coAwait()
         }
         return result.map { transform(it) }
     }
@@ -34,7 +40,7 @@ class CardioTransaction(val c: SqlConnection) {
         val result = if (args.isEmpty()) {
             c.query(stmt).execute().coAwait()
         } else {
-            c.preparedQuery(stmt).execute(Tuple.wrap(args)).coAwait()
+            c.preparedQuery(stmt).execute(args.toTuple()).coAwait()
         }
         return result.rowCount().toLong()
     }
