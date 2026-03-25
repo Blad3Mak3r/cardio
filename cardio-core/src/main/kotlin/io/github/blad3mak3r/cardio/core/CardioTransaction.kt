5cd1e4e461d5dc74e5d5c2cab7f2b16a791f807b
Blad3Mak3r <juanluiscarobenito@gmail.com>
2026-03-25 15:39:23 +0100

    feat: add CardioTransaction class for managing database transactions

---
+++ cardio-core/src/main/kotlin/io/github/blad3mak3r/cardio/core/CardioTransaction.kt
@@ -0,0 +1,29 @@
+package io.github.blad3mak3r.cardio.core
+
+import io.github.blad3mak3r.cardio.protocol.Row
+import io.github.blad3mak3r.cardio.protocol.codec.Param
+import io.github.blad3mak3r.cardio.protocol.connection.ConnectionPool
+
+class CardioTransaction internal constructor(
+    private val conn: ConnectionPool.PooledConnection,
+) {
+    suspend fun <T> query(
+        sql: String,
+        vararg params: Param<*>,
+        mapper: (Row) -> T,
+    ): List<T> = conn.query(sql, *params, mapper = mapper)
+
+    suspend fun <T> queryOne(
+        sql: String,
+        vararg params: Param<*>,
+        mapper: (Row) -> T,
+    ): T? = query(sql, *params, mapper = mapper).firstOrNull()
+
+    suspend fun execute(
+        sql: String,
+        vararg params: Param<*>,
+    ): Long = conn.execute(sql, *params)
+    
+    suspend fun commit() = conn.commitTransaction()
+    suspend fun rollback() = conn.rollbackTransaction()
+}
