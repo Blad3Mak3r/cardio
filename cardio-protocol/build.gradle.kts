526b0a17273761f80827555ac2371b820bc1fede
Blad3Mak3r <juanluiscarobenito@gmail.com>
2026-03-27 11:08:17 +0100

    feat: add Maven publishing support and configure project metadata for cardio-protocol

--- cardio-protocol/build.gradle.kts
+++ cardio-protocol/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.maven.publish)
+
+    `java-library`
 }
 
 dependencies {
@@ -9,4 +12,40 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+mavenPublishing {
+    coordinates("io.github.blad3mak3r.cardio", "cardio-protocol", "$version")
+
+    pom {
+        name.set("cardio-protocol")
+        description.set("Custom PostgreSQL wire protocol over Ktor sockets for Cardio, a coroutine-native PostgreSQL library for Kotlin")
+        url.set("https://github.com/Blad3Mak3r/cardio")
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/Blad3Mak3r/cardio/issues")
+        }
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://github.com/Blad3Mak3r/cardio/LICENSE.txt")
+                distribution.set("repo")
+            }
+        }
+        scm {
+            url.set("https://github.com/Blad3Mak3r/cardio")
+            connection.set("https://github.com/Blad3Mak3r/cardio.git")
+            developerConnection.set("scm:git:ssh://git@github.com:Blad3Mak3r/cardio.git")
+        }
+        developers {
+            developer {
+                name.set("Juan Luis Caro")
+                url.set("https://github.com/Blad3Mak3r")
+            }
+        }
+    }
+
+    publishToMavenCentral(automaticRelease = true)
+
+    signAllPublications()
 }
